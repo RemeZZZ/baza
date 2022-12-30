5a6cdb243d30d57c54c000ecf0aeb51952d9651e
Daniil <daniil.remezoff@yandex.ru>
2022-12-30 19:55:51 +0300

    url-fix

--- xlsxEditor/banks/alpha.js
+++ xlsxEditor/banks/alpha.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 export default async function check(inn, phone) {
-  const result = await fetch('localhost:3101/check', {
+  const result = await fetch('http://localhost:3101/check', {
     method: 'POST',
 
     headers: {
