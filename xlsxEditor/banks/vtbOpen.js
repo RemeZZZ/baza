0b5e366dc71453bd41e6125309fdba0c5c210341
Даниил Ремезов <daniil.r@prmnd.com>
2023-06-23 17:53:54 +0300

    alpha open

--- xlsxEditor/banks/vtbOpen.js
+++ xlsxEditor/banks/vtbOpen.js
@@ -1,35 +1,6 @@
 import fetch from 'node-fetch';
 
-const promisesQueue = [];
-
 export default async function check(inn) {
-  return new Promise((resolve) => {
-    promisesQueue.push({
-      id: inn,
-      end: resolve,
-    });
-
-    setTimeout(() => {
-      const index = promisesQueue.findIndex((item) => item.id === inn);
-
-      console.log(promisesQueue[index]);
-
-      if (promisesQueue[index]) {
-        promisesQueue[index].end({
-          result: 'Хз',
-        });
-
-        promisesQueue.splice(index, 1);
-      }
-    }, 9 * 1000);
-  });
-}
-
-setInterval(async () => {
-  if (!promisesQueue.length) {
-    return;
-  }
-
   try {
     const result = await fetch('http://localhost:3102/check', {
       method: 'POST',
@@ -40,30 +11,12 @@ setInterval(async () => {
       },
 
       body: JSON.stringify({
-        leads: promisesQueue.map((item) => {
-          return {
-            inn: item.id,
-          };
-        }),
+        leads: [{ inn }],
       }),
     });
 
     const data = await result.json();
 
-    if (data.leads) {
-      data.leads.forEach((lead) => {
-        const index = promisesQueue.findIndex(
-          (item) => `${item.id}` === `${lead.inn}`,
-        );
-
-        const promise = promisesQueue[index];
-
-        if (promise) {
-          promise.end(lead);
-
-          promisesQueue.splice(index, 1);
-        }
-      });
-    }
+    return data;
   } catch {}
-}, 500);
+}
